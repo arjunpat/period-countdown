488147a75facc177fdf2b653ecd21134e69192d5
Arjun Patrawala <ajpat1234@gmail.com>
2018-06-29 00:00:15 -0700

    [client] [js] analytics class is fully functional

--- public/js/Analytics.js
+++ public/js/Analytics.js
@@ -5,47 +5,50 @@ class Analytics {
 		this.sent = false;
 	}
 
-	a() { // checks data values and sends if all are there
+	async a() { // checks data values and sends if all are there
 
-		let speedInfo = window.performance.timing;
-		let data = {
-			device_id: this.device_id,
-			speed: {
-				page_complete: speedInfo.loadEventEnd - speedInfo.navigationStart,
-				response_time: speedInfo.responseEnd - speedInfo.requestStart,
-				dom_complete: speedInfo.domComplete - speedInfo.domLoading,
-				request_length: speedInfo.requestStart - speedInfo.responseEnd,
-        		dns: speedInfo.domainLookupEnd - speedInfo.domainLookupStart,
-			},
-			referer: window.document.referrer
-		}
-
-		page_complete: speedInfo.loadEventEnd - speedInfo.navigationStart,
-
-        //responseTime
-        responseTime: speedInfo.responseEnd - speedInfo.requestStart,
-
-        //page render time
-        domComplete: speedInfo.domComplete - speedInfo.domLoading,
-
-        //dns lookup
-        dns: speedInfo.domainLookupEnd - speedInfo.domainLookupStart,
-
-        //time to first byte
-        ttfb: speedInfo.responseStart - speedInfo.navigationStart,
-
-        //time to interactive
-        tti: speedInfo.domInteractive - speedInfo.domLoading
-
+		let data;
 		if (this.pathname === '/' && this.device_id && this.theme && this.period && this.period_name) { // index page
-			data.prefs = {
-				theme: this.theme,
-				period: this.period
+
+			await this.sleep(5);
+			let speedInfo = window.performance.timing;
+			data = {
+				device_id: this.device_id,
+				speed: {
+					page_complete: speedInfo.loadEventEnd - speedInfo.navigationStart,
+					response_time: speedInfo.responseEnd - speedInfo.requestStart,
+					dom_complete: speedInfo.domComplete - speedInfo.domLoading,
+					dns: speedInfo.domainLookupEnd - speedInfo.domainLookupStart,
+					ttfb: speedInfo.responseStart - speedInfo.navigationStart,
+					tti: speedInfo.domInteractive - speedInfo.domLoading
+				},
+				referer: window.document.referrer,
+				prefs: {
+					theme: this.theme,
+					period: this.period
+				}
 			}
 			if (this.period !== this.period_name) data.prefs.period_name = this.period_name;
 		} else if (this.pathname && this.device_id && this.theme) {
 
-		//} else if (this.pathname && this.pathname !== '/settings' && this.pathname !== '/' && this.device_id && this.referer && this.theme) {
+			await this.sleep(5);
+			let speedInfo = window.performance.timing;
+			data = {
+				device_id: this.device_id,
+				speed: {
+					page_complete: speedInfo.loadEventEnd - speedInfo.navigationStart,
+					response_time: speedInfo.responseEnd - speedInfo.requestStart,
+					dom_complete: speedInfo.domComplete - speedInfo.domLoading,
+					request_length: speedInfo.requestStart - speedInfo.responseEnd,
+	        		dns: speedInfo.domainLookupEnd - speedInfo.domainLookupStart,
+	        		ttfb: speedInfo.responseStart - speedInfo.navigationStart,
+	        		tti: speedInfo.domInteractive - speedInfo.domLoading
+				},
+				prefs: {
+					theme: this.theme
+				},
+				referer: window.document.referrer
+			}
 
 		} else return;
 
@@ -54,6 +57,7 @@ class Analytics {
 		RequestManager.sendAnalytics(data).then(data => {
 			if (data.success) {
 				this.sent = true;
+				console.log('[Analytics] analytics data sent!');
 			}
 		});
 
@@ -87,5 +91,11 @@ class Analytics {
 	setRegisteredTo(x) {
 		this.registered_to = x;
 		this.a();
+	}
+
+	sleep(seconds) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => resolve(), seconds * 1e3);
+		});
 	}
 }
