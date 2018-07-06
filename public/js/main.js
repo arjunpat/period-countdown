6725a9a2ae7d9b838cc208b8bf48476f3aead5d8
Arjun Patrawala <ajpat1234@gmail.com>
2018-07-06 00:22:41 -0700

    [client] [js] huge settings update

--- public/js/main.js
+++ public/js/main.js
@@ -2,159 +2,165 @@
 var timingEngine, elements = new Elements(), analytics = new Analytics, prefManager = new PrefManager;
 
 
-var render = {
-	index: () => {
-		Logger.time('main', 'index');
+var render = {};
+render.index = () => {
+	Logger.time('main', 'index');
 
-		elements.switchTo('index');
+	elements.switchTo('index');
 
-		let firstRun = true;
-		var mainLoop = () => {
+	let firstRun = true;
+	var mainLoop = () => {
 
-			if (window.location.pathname === '/') {
+		if (window.location.pathname === '/') {
 
-				let time = timingEngine.getRemainingTime();
+			let time = timingEngine.getRemainingTime();
 
-				time.period_name = prefManager.getPeriodName(time.period) || time.period;
+			time.period_name = prefManager.getPeriodName(time.period) || time.period;
 
-				elements.updateScreen(time);
+			elements.updateScreen(time);
 
-				if (firstRun) {
-					window.onresize();
-					analytics.setPeriod(time.period);
-					analytics.setPeriodName(time.period_name);
-					firstRun = false;
-				}
-
+			if (firstRun) {
+				window.onresize();
+				analytics.setPeriod(time.period);
+				analytics.setPeriodName(time.period_name);
+				firstRun = false;
 			}
 
-			setTimeout(mainLoop, 50);
 		}
 
-		if (!timingEngine) {
-			// startup the actually timer; only happens when u actually go to the index page
-			Promise.all([RequestManager.getPresets(), RequestManager.getCalendar()]).then(values => {
-				let [presets, calendar] = values;
+		setTimeout(mainLoop, 50);
+	}
 
-				timingEngine = new TimingEngine(presets, calendar);
-				
-				elements.updateElementsWithPreferences(prefManager.getAllPreferences()); // before first paint
-				mainLoop();
-				elements.updateScreenFontSize();
-				//elements.hidePreloader();
+	if (!timingEngine) {
+		// startup the actually timer; only happens when u actually go to the index page
+		Promise.all([RequestManager.getPresets(), RequestManager.getCalendar()]).then(values => {
+			let [presets, calendar] = values;
+
+			timingEngine = new TimingEngine(presets, calendar);
 			
-				Logger.timeEnd('main', 'index');
-
-			}).catch(err => {
-				//elements.showErrorScreen();
-				RequestManager.sendError({
-					where: 'browser',
-					type: 'client_page_load',
-					description: err.stack
-				});
-			});
-
-			window.onresize = () => {
-				elements.updateScreenFontSize();
-				elements.dimensionCanvas();
-			}
-
-			elements.index.settingsButton.querySelector('div').onclick = () => {
-				load('/settings', true);
-
-				if (!prefManager.isLoggedIn())
-					setTimeout(() => elements.showModal('log-in-first'), 2000);
-			}
-
-			elements.index.googleSignin.querySelector('div').onclick = () => {
-				elements.addGoogleApi();
-				elements.showModal('modal-profile-options');
-			}
-
-			document.querySelector('#modal .modal-profile-options > button').onclick = () => {
-				Promise.all([gapi.auth2.getAuthInstance().signOut(), RequestManager.logout()]).then(values => {
-					Storage.clearAllExceptDeviceId();
-					window.location.reload();
-				});
-			}
-		} else
+			elements.applyPreferencesToElements(prefManager.getAllPreferences()); // before first paint
+			mainLoop();
+			elements.updateScreenFontSize();
+			//elements.hidePreloader();
+		
 			Logger.timeEnd('main', 'index');
 
-	},
-	settings: () => {
+		}).catch(err => {
+			//elements.showErrorScreen();
+			RequestManager.sendError({
+				where: 'browser',
+				type: 'client_page_load',
+				description: err.stack
+			});
+		});
 
-		Logger.time('main', 'settings');
+		window.onresize = () => {
+			elements.updateScreenFontSize();
+			elements.dimensionCanvas();
+		}
 
-		// webpage display
-		document.title = 'Settings - Bell Countdown';
-		elements.switchTo('settings');
+		elements.index.settingsButton.querySelector('div').onclick = () => {
+			load('/settings', true);
 
-		// animation
-		elements.settings.title.classList.remove('underlineAnimation');
-		setTimeout(() => elements.settings.title.classList.add('underlineAnimation'), 20);
+			if (!prefManager.isLoggedIn())
+				setTimeout(() => elements.showModal('log-in-first'), 2000);
+		}
 
-		// form stuff
-		if (elements.settings.inputs[0].onkeyup === null)
+		elements.index.googleSignin.querySelector('div').onclick = () => {
+			elements.addGoogleApi();
+			elements.showModal('modal-profile-options');
+		}
+
+		document.querySelector('#modal .modal-profile-options > button').onclick = () => {
+			Promise.all([gapi.auth2.getAuthInstance().signOut(), RequestManager.logout()]).then(values => {
+				Storage.clearAllExceptDeviceId();
+				window.location.reload();
+			});
+		}
+	} else
+		Logger.timeEnd('main', 'index');
+
+}
+render.settings = () => {
+
+	Logger.time('main', 'settings');
+
+	// webpage display
+	document.title = 'Settings - Bell Countdown';
+	elements.switchTo('settings');
+
+	// animation
+	elements.settings.title.classList.remove('underlineAnimation');
+	setTimeout(() => elements.settings.title.classList.add('underlineAnimation'), 20);
+
+	// form stuff
+	if (elements.settings.closeButton.onclick === null) {
+
+		var savePeriodNames = () => {
+			let names = {};
 			for (let element of elements.settings.inputs) {
-				let period_num = element.id.substring(6, 7);
+				let num = element.id.substring(6, 7);
+				names[num] = element.value.trim();
+			}
 
-				element.onkeyup = element.onblur = () => {
-					if (element.value !== prefManager.getPeriodName(period_num)) {
-						if (element.value.length > 0)
-							element.classList.add('has-value');
-						else
-							element.classList.remove('has-value');
-						elements.settingChangesNotSaved();
-					}
+			prefManager.setPeriodNames(names).then(val => {
+				if (val)
+					setTimeout(() => {
+						elements.settingChangesSaved();
+					}, 1000);
+				else
+					window.alert('not saving'); // TODO
+			});
+		}
+
+		elements.settings.closeButton.onclick = () => {
+			if (!elements.settings.saved)
+				savePeriodNames();
+			load('/', true);
+		}
+
+		for (let element of elements.settings.inputs) {
+			let period_num = parseInt(element.id.substring(6, 7));
+
+			element.onblur = () => {
+				element.onkeyup();
+
+				if (element.value !== prefManager.getPeriodName(period_num) && !(element.value === '' && prefManager.getPeriodName(period_num) === undefined))
+					savePeriodNames();
+
+			}
+			element.onkeyup = () => {
+				if (element.value !== prefManager.getPeriodName(period_num)) {
+					if (element.value.length > 0)
+						element.classList.add('has-value');
+					else
+						element.classList.remove('has-value');
+					elements.settingChangesNotSaved();
 				}
 			}
+		}
+		elements.settings.themeSelector.onchange = () => {
+			let val = elements.settings.themeSelector.value;
 
-	
-		if (elements.settings.saveSettingsButton.onclick === null)
-			elements.settings.saveSettingsButton.onclick = () => {
-				let elem = elements.settings.saveSettingsButton;
-				let currentText = elem.innerHTML;
-				elem.innerHTML = 'Saving...';
-				elem.disabled = 'true';
+			prefManager.setThemeByName(val);
 
-				let names = {};
+			elements.applyPreferencesToElements(prefManager.getAllPreferences());
 
-				for (let element of elements.settings.inputs) {
-					let num = parseInt(element.id.substring(6, 7));
-					element.value = element.value.trim();
-					names[num] = element.value;
-				}
-
-				prefManager.setPeriodNames(names);
-
-				setTimeout(() => {
-					elem.disabled = '';
-					elem.innerHTML = currentText;
-					elements.settingChangesSaved();
-				}, 2000);
-
-			}
-
-		if (elements.settings.closeButton.onclick === null)
-			elements.settings.closeButton.onclick = () => {
-				if (!elements.settings.saved)
-					elements.settings.saveSettingsButton.click();
-				load('/', true);
-			}
-
-		elements.fillPeriodNameInputs(prefManager.getAllPreferences().period_names);
-
-
-		Logger.timeEnd('main', 'settings');
-	},
-	notFound: () => {
-		Logger.time('main', 'notFound');
-
-		document.title = 'Not Found - Bell Countdown';
-		elements.switchTo('not-found');
-
-		Logger.timeEnd('main', 'notFound');
+		}
 	}
+
+	elements.applyPreferencesToElements(prefManager.getAllPreferences());
+
+
+	Logger.timeEnd('main', 'settings');
+}
+render.notFound = () => {
+	Logger.time('main', 'notFound');
+
+	document.title = 'Not Found - Bell Countdown';
+	elements.switchTo('not-found');
+
+	Logger.timeEnd('main', 'notFound');
 }
 
 var load = (path, shouldPushHistory = false) => {
@@ -175,13 +181,13 @@ var load = (path, shouldPushHistory = false) => {
 
 
 // inital page render
-load(window.location.pathname, false);
+load(window.location.pathname);
 
 RequestManager.init().then(data => {
 
 	if (data.email) {
 		prefManager.setGoogleAccount(data);
-		elements.updateElementsWithPreferences(prefManager.getAllPreferences());
+		elements.applyPreferencesToElements(prefManager.getAllPreferences());
 		analytics.setRegisteredTo(data.email);
 	} else
 		elements.addGoogleApi();
@@ -207,7 +213,7 @@ RequestManager.init().then(data => {
 
 // makes sure that back and forwards buttons work
 window.onpopstate = () => {
-	load(window.location.pathname, false);
+	load(window.location.pathname);
 }
 
 // sends analytics
@@ -256,7 +262,7 @@ var googleApiDidLoad = () => {
 						window.alert('Our servers are having a bad day. Please try again another time.');
 					}
 					
-					elements.updateElementsWithPreferences(prefManager.getAllPreferences());
+					elements.applyPreferencesToElements(prefManager.getAllPreferences());
 				}).catch(err => {
 					// OTHER CASE
 				});
@@ -266,8 +272,8 @@ var googleApiDidLoad = () => {
 				window.alert('There was a problem signing you in. Please try again later.');
 			}
 
-			GoogleAuth.attachClickHandler(elements.index.googleSignin.querySelector('button'), {}, gSuccess, gFail);
-			GoogleAuth.attachClickHandler(document.querySelector('.log-in-first a'), {}, gSuccess, gFail);
+			for (let element of document.getElementsByClassName('google-login-button'))
+				GoogleAuth.attachClickHandler(element, {}, gSuccess, gFail);
 		});
 	});
 
