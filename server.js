68af8836ebf185dff513d455deda042356ac3490
Arjun Patrawala <ajpat1234@gmail.com>
2018-08-12 16:07:40 -0700

    enabled .env and pm2

--- server.js
+++ server.js
@@ -6,18 +6,13 @@
 
 'use strict';
 
-//process.env.NODE_ENV = 'production';
+require('dotenv').config();
+
 console.time('startup');
 const http = require('http');
-
-// cache of local files
-const cache = require('./app/cache.js');
-
-// homemade utility class
-const utils = require('./app/utils.js');
-
-// entire api
-const api = require('./app/api/api.js');
+const cache = require('./lib/cache.js');
+const utils = require('./lib/utils.js');
+const api = require('./lib/api/api.js');
 
 const server = http.createServer((req, res) => {
 
@@ -25,20 +20,15 @@ const server = http.createServer((req, res) => {
 	//console.log(path);
 
 	if (path.layers[0] === 'api') {
-
 		api(req, res, path).then(data => {
 			if (data.valid) {
-
 				res.writeHead(200, data.headers);
 				res.end(data.content);
-
 			} else {
-
 				res.end(JSON.stringify({
 					success: false,
 					error: 'bad_request'
 				}));
-
 			}
 		}).catch(err => {
 			res.end(JSON.stringify({
@@ -49,20 +39,15 @@ const server = http.createServer((req, res) => {
 		});
 
 	} else { // if is a static file
-
 		cache.getFile(path.path).then(file => {
 			res.writeHead(200, utils.mergeHeaders(file.headers));
 			res.end(file.content);
-
 		}).catch((err) => {
 			console.log(err);
-
 			// handle 404 errors
 			cache.getFile('/').then(file => { res.writeHead(404, utils.mergeHeaders(file.headers));res.end(file.content) });
 		});
-
 	}
-
 
 }).listen(8080, () => {
 	console.timeEnd('startup');
