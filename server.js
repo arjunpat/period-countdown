1f592d4e0bc76a9eb613b3009d5f333c8a66e64d
Arjun Patrawala <ajpat1234@gmail.com>
2018-12-18 18:49:33 -0800

    caved in and converted to express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
  * period-countdown
- * Copyright(c) 2018 Arjun Patrawala
+ * Copyright(c) 2019 Arjun Patrawala
  * MIT Licensed
  */
 
@@ -9,48 +9,24 @@
 require('dotenv').config();
 
 console.time('startup');
-const http = require('http');
-const cache = require('./lib/cache.js');
-const utils = require('./lib/utils.js');
-const api = require('./lib/api/api.js');
+const express = require('express');
+const app = express();
+const utils = require('./lib/utils');
+const cache = require('./lib/cache');
 
-const server = http.createServer((req, res) => {
+app.use(express.json());
+app.use('/api', require('./lib/api/api'));
 
-	let path = utils.parseURL(req.url);
-	//console.log(path);
+app.all('*', (req, res) => {
 
-	if (path.layers[0] === 'api') {
-		api(req, res, path).then(data => {
-			if (data.valid) {
-				res.writeHead(200, utils.mergeHeaders(data.headers));
-				res.end(data.content);
-			} else {
-				res.writeHead(200, utils.mergeHeaders({}));
-				res.end(JSON.stringify({
-					success: false,
-					error: 'bad_request'
-				}));
-			}
-		}).catch(err => {
-			res.writeHead(200, utils.mergeHeaders({}));
-			res.end(JSON.stringify({
-				success: false,
-				error: 'bad_request'
-			}));
-			console.log(err);
-		});
-
-	} else { // if is a static file
-		cache.getFile(path.path).then(file => {
-			res.writeHead(200, utils.mergeHeaders(file.headers));
-			res.end(file.content);
-		}).catch((err) => {
-			console.log(err);
-			// handle 404 errors
-			cache.getFile('/').then(file => { res.writeHead(200, utils.mergeHeaders(file.headers)); res.end(file.content); });
-		});
-	}
-
-}).listen(process.env.PORT, () => {
-	console.timeEnd('startup');
+	// i don't like express's servestatic function
+	cache.getFile(req.path).then(file => {
+		res.set(utils.mergeHeaders(file.headers));
+		res.send(file.content);
+	}).catch(err => {
+		console.log(err);
+		cache.getFile('/').then(file => { res.set(utils.mergeHeaders(file.headers)); res.send(file.content); });
+	});
 });
+
+app.listen(process.env.PORT, () => console.timeEnd('startup'));
